b6f3e62a655c43aac046e51187069cb7f28e1ee7
Maddox Guthrie <m4dpup@gmail.com>
2026-02-23 11:16:47 -0600

    Add presentation function to deliverables and start work on adding sprint 1 deliverables

--- src/pages/Deliverables.tsx
+++ src/pages/Deliverables.tsx
@@ -1,23 +1,35 @@
+function Presentation({title, embed, download}) {
+    return (
+        <section>
+            <h2 className="deliverable_title">{title}</h2>
+
+            <div className="slide-container">
+                <iframe src={embed} allowFullScreen/>
+            </div>
+
+            <p>
+                <a href={download} target="_blank" rel="noreferrer">
+                    Download Presentation (PDF)
+                </a>
+            </p>
+        </section>
+    );
+}
+
 export default function Deliverables() {
-  return (
-    <div className="page">
-      <h2 className="deliverable_title">Preliminary Presentation</h2>
+    return (
+        <div className="page">
+            <Presentation
+                title="Preliminary Presentation"
+                embed="https://docs.google.com/presentation/d/e/2PACX-1vQ7iPyGsCWZtszLqBkkMMru4Z_b7P4M8dV5eD_pMe03QGsSW9viGgUd-X0IyPfI5_NA7rkIsao1E-Ru/pubembed?start=false&loop=false&delayms=3000"
+                download="https://drive.google.com/uc?export=download&id=1DQ0uR8j9YY9Xgn2_FMX8tIKahq4LXAfV"
+            />
 
-      <div className="slide-container">
-        <iframe
-          src="https://docs.google.com/presentation/d/e/2PACX-1vQ7iPyGsCWZtszLqBkkMMru4Z_b7P4M8dV5eD_pMe03QGsSW9viGgUd-X0IyPfI5_NA7rkIsao1E-Ru/pubembed?start=false&loop=false&delayms=3000"
-          allowFullScreen
-        ></iframe>
-      </div>
-
-      <p>
-        <a
-          href="https://drive.google.com/uc?export=download&id=1DQ0uR8j9YY9Xgn2_FMX8tIKahq4LXAfV"
-          target="_blank"
-        >
-          Download Presentation (PDF)
-        </a>
-      </p>
-    </div>
-  );
+            <Presentation
+                title="Sprint 1 Presentation"
+                embed="https://docs.google.com/presentation/d/e/2PACX-1vR_O_D6YvtTk5SqFLtd2HssH2Vonev0dZvjUO9Ri_hi5OtDNQb9uTZtYDBtNxrfmbDdTkHd_0Fm77Dd/pubembed?start=false&loop=false&delayms=3000"
+                download=""
+            />
+        </div>
+    );
 }
